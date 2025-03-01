dd6d29166b71389837c2e141f8e17e8415fab4b3
Shelbyen <ars.sheberstov@mail.ru>
2025-03-02 04:16:12 +0500

    fix some bugs

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -1,9 +1,9 @@
+import asyncio
 import os
 from urllib.error import URLError
 from urllib.parse import urlparse
 
 from aiogram import Router, F
-from aiogram.enums import ContentType
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, FSInputFile
@@ -14,6 +14,18 @@ from src.yt_download.downloader import downloader
 
 router = Router()
 all_media_dir = 'res/yt-dir'
+
+
+async def send_progress(message: Message):
+    while (video_id := downloader.download_now_id.get(message.from_user.id)) == 'starting':
+        await asyncio.sleep(1)
+
+    percent = 0
+    while video_id and (status := downloader.download_now[video_id]) != 'done':
+        await asyncio.sleep(1)
+        if percent != status:
+            percent = status
+            await message.edit_text(f'Процесс: {percent:.1f}%')
 
 
 class DownloadingVideo(StatesGroup):
@@ -37,8 +49,14 @@ async def get_link(message: Message):
         await message.answer(i18n.translate(message, 'wrong_link'))
         return
 
-    file_name = downloader.download(message.text)
-    with ChatActionSender.upload_video(message.from_user.id, message.bot):
+    progress_message = await message.answer(i18n.translate(message, 'starting_download'))
+
+    # downloader.download_now_id[message.from_user.id] = 'starting'
+    # tt = await asyncio.gather(send_progress(message), downloader.download(message.text, progress_message))
+    file_name, yt_id = await downloader.download(message.text, progress_message)
+    async with ChatActionSender.upload_video(message.from_user.id, message.bot):
         video_file = FSInputFile(path=os.path.join(all_media_dir, file_name))
         msg = await message.answer_video(video_file, caption=file_name[:-4], supports_streaming=True)
+
     video_file_id = msg.video.file_id
+
--- src/yt_download/downloader.py
+++ src/yt_download/downloader.py
@@ -1,11 +1,17 @@
+import asyncio
 from os import listdir
 
+from aiogram.types import Message
 from yt_dlp import YoutubeDL
 
 
 def progress_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now post-processing ...')
+    if d['status'] == 'downloading' and d.get('total_bytes_estimate') and d.get('downloaded_bytes'):
+        pr = d['downloaded_bytes'] / d['total_bytes_estimate']
+        downloader.download_now[d['info_dict']['id']] = pr
+    else:
+        downloader.download_now[d['info_dict']['id']] = 'done'
+    asyncio.get_event_loop().create_task(asyncio.sleep(0.01))
 
 
 def shorted_than_a_time(info, *, incomplete):
@@ -18,7 +24,7 @@ ydl_opts = {
     'format': 'bv[width<=1920][ext=mp4]+ba[ext=m4a]',
     'merge_output_format': 'mp4',
     'socket_timeout': 60,
-    'progress_hooks': [progress_hook],
+    # 'progress_hooks': [progress_hook],
     'match_filter': shorted_than_a_time,
     'paths': {'home': 'res/yt-dir', 'temp': 'temp'},
     'quiet': True
@@ -33,17 +39,23 @@ def get_video_id(url) -> str:
 
 class Downloader:
     def __init__(self):
-        self.ydl = YoutubeDL(ydl_opts)
+        self.download_now = {}
+        self.download_now_id = {}
 
-    def download(self, url: str) -> str:
+    async def download(self, url: str, message: Message) -> tuple[str, str]:
         video_id = get_video_id(url)
-        error_code = self.ydl.download(url)
+        # self.download_now_id[message.from_user.id] = video_id
+        # self.download_now[video_id] = 0
+
+        ydl = YoutubeDL(ydl_opts)
+
+        error_code = ydl.download(url)
         if error_code:
             raise Exception('Video failed to download')
 
         for file_name in listdir('res/yt-dir'):
             if file_name.endswith(f'[{video_id}].mp4'):
-                return file_name
+                return file_name, video_id
 
 
 downloader = Downloader()
