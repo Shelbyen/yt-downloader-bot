e94e197934b19640f2794817202503c99272f685
Shelbyen <ars.sheberstov@mail.ru>
2025-03-01 18:10:54 +0500

    add action when sending

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -5,7 +5,8 @@ from urllib.parse import urlparse
 from aiogram import Router
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile, InputMediaVideo
+from aiogram.utils.chat_action import ChatActionSender
 
 from src.i18n.i18n import i18n
 from src.yt_download.downloader import downloader
@@ -36,5 +37,6 @@ async def get_link(message: Message):
         return
 
     file_name = downloader.download(message.text)
-    video_file = FSInputFile(path=os.path.join(all_media_dir, file_name))
-    await message.bot.send_video(message.from_user.id, video=video_file, caption=file_name[:-4])
+    with ChatActionSender.upload_video(message.from_user.id, message.bot):
+        video_file = FSInputFile(path=os.path.join(all_media_dir, file_name))
+        await message.answer_video(video_file, caption=file_name[:-4])
