fc08c176774f01c83baa25171b710d601141b6a9
Shelbyen <ars.sheberstov@mail.ru>
2025-03-02 05:41:01 +0500

    add sqlalchemy and model

---
+++ src/models/video_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base_model import Base
+
+
+class PhotoModel(Base):
+    __tablename__ = "videos"
+
+    id: Mapped[str] = mapped_column(String, primary_key=True)
+    file_id: Mapped[str]
